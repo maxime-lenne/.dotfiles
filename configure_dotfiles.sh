de288f928321c867988446a33dfea9a5e8bfeefc
Maxime Lenne <maxime.lenne@gmail.com>
2022-08-03 10:25:38 +0200

    change configure dotfiles

--- configure_dotfiles.sh
+++ configure_dotfiles.sh
@@ -4,10 +4,13 @@
 # Symlink each dotfile in the home directory
 # TODO : Backup existing dotfile first
 
-for name in .{aliases,bash_profile,bash_prompt,bashrc,exports,functions,gitconfig,gitignore_global,zshrc,hyper}; do
+for name in .{aliases,bash_profile,bash_prompt,bashrc,exports,functions,gitconfig,gitignore_global,zshrc}; do
   source="$PWD/$name"
   target="$HOME/$name"
   mv "$target" "$target.backup"
   echo "-----> Symlinking $source to $target"
   ln -s "$source" "$target"
 done
+
+echo "-----> Copy .dotfiles/.hyper.js to .hyper.js
+cp ".dotfiles/.hyper.js" ".hyper.js"
